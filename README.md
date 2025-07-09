b19d18b81baa9b904c5511823ce7e6abb9963a8f
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-07-09 19:10:34 +0530

    initial commit, create files and folders

---
+++ README.md
@@ -0,0 +1,2 @@
+# React + Vite
+This is dark mode mini-project by using react js.
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div className="page">
+        
+    </div>
+  )
+}
+
+export default Blog
